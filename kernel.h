fd46505a1d3ba9d25e1764de23cad40ad8b791bc
NumberFour8 <lukas.pohanka@inina.net>
2014-04-12 20:14:35 +0200

    Prejmenovani metod

--- kernel.h
+++ kernel.h
@@ -7,8 +7,8 @@
 #include "coords.h"
 #include "helpers.h"
 
-#define NUM_BLOCKS 1
-#define CURVES_PER_BLOCK 4
+#define NUM_BLOCKS 2
+#define CURVES_PER_BLOCK 2
 
 #define NUM_CURVES NUM_BLOCKS*CURVES_PER_BLOCK
 
